55411845f1f4dda84502c86b018bfb0fd98819fa
spttmssla55 <spttmssla555@naver.com>
2025-03-17 16:08:14 +0900

    mobileSoftware - Java

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
